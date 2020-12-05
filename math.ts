602f781f0958be71ecebb9a52acf9094726beaae
Rio6 <rio.liu@r26.me>
2020-12-04 20:42:05 -0500

    3D rendering

---
+++ math.ts
@@ -0,0 +1,174 @@
+/*
+ * math for mul, frustum, perspective, and rotate3D from
+ * https://github.com/treeform/vmath
+ */
+
+type Vec3 = [number, number, number];
+type Vec4 = [number, number, number, number];
+type Mat4 = [
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number,
+    number, number, number, number,
+];
+
+function zero_mat(): Mat4 {
+    return [
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+    ];
+}
+
+function identity(): Mat4 {
+    return [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+    ];
+}
+
+function mul(...mats: Mat4[]): Mat4 {
+    const do_mul = (a: Mat4, b: Mat4): Mat4 => {
+        const [
+            a00, a01, a02, a03,
+            a10, a11, a12, a13,
+            a20, a21, a22, a23,
+            a30, a31, a32, a33,
+        ] = a;
+
+        const [
+            b00, b01, b02, b03,
+            b10, b11, b12, b13,
+            b20, b21, b22, b23,
+            b30, b31, b32, b33,
+        ] = b;
+
+        return [
+            b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+            b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+            b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+            b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+            b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+            b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+            b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+            b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+            b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+            b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+            b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+            b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+            b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+            b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+            b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+            b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+        ];
+    };
+
+    return mats.reduceRight((a, b) => do_mul(b, a));
+}
+
+function frustum(left: number, right: number, bottom: number, top: number, near: number, far: number): Mat4 {
+    const rl = (right - left);
+    const tb = (top - bottom);
+    const fn = (far - near);
+
+    return [
+        (near*2) / rl, 0, 0, 0,
+        0, (near*2) / tb, 0, 0,
+        (right + left) / rl, (top + bottom) / tb, -(far + near) / fn, -1, 0,
+        0, -(far*near*2) / fn, 0,
+    ]
+}
+
+function perspective(fovy: number, aspect: number, near: number, far: number): Mat4 {
+    const top = near * Math.tan(fovy*Math.PI / 360.0);
+    const right = top * aspect;
+    return frustum(-right, right, -top, top, near, far);
+}
+
+function translate(x: number, y: number, z: number): Mat4 {
+    return [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        x, y, z, 1,
+    ];
+}
+
+function scale(f: number): Mat4 {
+    return [
+        f, 0, 0, 0,
+        0, f, 0, 0,
+        0, 0, f, 0,
+        0, 0, 0, 1,
+    ];
+}
+
+function rotate3D(th: number, axis: Vec3): Mat4 {
+    const s = Math.sin(th/2);
+    const c = Math.cos(th/2);
+
+    let
+        x = s * axis[0],
+        y = s * axis[1],
+        z = s * axis[2],
+        w = c,
+
+        xx = x * x,
+        xy = x * y,
+        xz = x * z,
+        xw = x * w,
+        yy = y * y,
+        yz = y * z,
+        yw = y * w,
+        zz = z * z,
+        zw = z * w;
+
+    return [
+        1 - 2 * (yy + zz),
+        0 + 2 * (xy - zw),
+        0 + 2 * (xz + yw),
+        0,
+        0 + 2 * (xy + zw),
+        1 - 2 * (xx + zz),
+        0 + 2 * (yz - xw),
+        0,
+        0 + 2 * (xz - yw),
+        0 + 2 * (yz + xw),
+        1 - 2 * (xx + yy),
+        0,
+        0, 0, 0, 1,
+    ];
+}
+
+function rotate4D(th: number): Mat4 {
+    const s = Math.sin(th);
+    const c = Math.cos(th);
+
+    return [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, c, s,
+        0, 0, -s, c,
+    ]
+}
+
+function project2D(): Mat4 {
+    return [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+    ];
+}
+
+function project3D(w): Mat4 {
+    return [
+        1/(1+w), 0, 0, 0,
+        0, 1/(1+w), 0, 0,
+        0, 0, 1/(1+w), 0,
+        0, 0, 0, 0,
+    ];
+}
---
+++ ttt.ts
@@ -0,0 +1,265 @@
+/// <reference path="./model_data.ts" />
+/// <reference path="./math.ts" />
+
+const config = {
+    fov: 60,
+    rot_speed: 0.01,
+    zoom_speed: 0.25,
+};
+
+window.onload = init;
+
+const vertex_shader_src = `
+    precision mediump float;
+
+    uniform vec4 model_pos;
+    uniform mat4 transform;
+    uniform mat4 rotation;
+    uniform mat4 projection;
+
+    attribute vec4 position;
+    attribute vec4 next_position;
+
+    void main() {
+        vec4 projected = projection * rotation * (position + model_pos);
+        projected.w = 1.0;
+        gl_Position = transform * projected;
+    }
+`;
+
+const frag_shader_src = `
+    precision mediump float;
+
+    uniform vec4 color;
+
+    void main() {
+        gl_FragColor = color;
+    }
+`;
+
+enum Piece {
+    EMPTY, X, O
+}
+
+enum Dimension {
+    TWO, THREE, FOUR
+}
+
+type Board = {
+    dimension: number,
+    pieces: Piece[] | Piece[][] | Piece[][][] | Piece[][][][];
+}
+
+class Model {
+    private vbo: WebGLBuffer;
+    private vao: WebGLVertexArrayObject;
+    private ebo: WebGLBuffer;
+    private color: Vec4;
+    private elem_count: number;
+
+    constructor({color, elems, verts}: {color: Vec4, elems: number[], verts: number[]}) {
+        const gl = ctx.gl;
+
+        this.color = color;
+        this.elem_count = elems.length;
+
+        this.vbo = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+        this.vao = gl.createVertexArray();
+        gl.bindVertexArray(this.vao);
+        gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(0);
+
+        this.ebo = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ebo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int32Array(elems), gl.STATIC_DRAW);
+    }
+
+    draw(position: Vec4) {
+        const gl = ctx.gl;
+
+        gl.uniform4fv(ctx.uniform.model_pos, position);
+        gl.uniform4fv(ctx.uniform.color, this.color);
+        gl.bindVertexArray(this.vao);
+        gl.drawElements(gl.LINES, this.elem_count, gl.UNSIGNED_INT, 0);
+    }
+}
+
+let models: {
+    [key in Dimension]: {
+        pieces: {
+            [Piece.X]: Model
+            [Piece.O]: Model
+        }
+        grid: Model
+    }
+};
+
+let ctx: {
+    gl: WebGL2RenderingContext,
+
+    uniform: {
+        model_pos:  WebGLUniformLocation,
+        transform: WebGLUniformLocation,
+        rotation:   WebGLUniformLocation,
+        projection: WebGLUniformLocation,
+        color:      WebGLUniformLocation,
+    }
+};
+
+const camera = {
+    rotation: [
+        0, 0, 0
+    ],
+    distance: 2,
+    dimension: Dimension.THREE
+};
+
+const board = {
+    dimension: Dimension.THREE,
+    pieces: []
+}
+
+function init() {
+    const can = document.getElementById("canvas") as HTMLCanvasElement;
+    const gl = can.getContext('webgl2');
+
+    can.addEventListener('mousemove', mousemove);
+    can.addEventListener('wheel', wheel);
+    can.addEventListener('contextmenu', e => {
+        e.preventDefault();
+        return false
+    });
+
+    const vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertex_shader, vertex_shader_src);
+    gl.compileShader(vertex_shader);
+    if(!gl.getShaderParameter(vertex_shader, gl.COMPILE_STATUS))
+        throw gl.getShaderInfoLog(vertex_shader);
+
+    const frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(frag_shader, frag_shader_src);
+    gl.compileShader(frag_shader);
+    if(!gl.getShaderParameter(frag_shader, gl.COMPILE_STATUS))
+        throw gl.getShaderInfoLog(frag_shader);
+
+    const program = gl.createProgram();
+    gl.attachShader(program, vertex_shader);
+    gl.attachShader(program, frag_shader);
+    gl.linkProgram(program);
+    if(!gl.getProgramParameter(program, gl.LINK_STATUS))
+        throw gl.getProgramInfoLog(program);
+
+    gl.useProgram(program);
+
+    ctx = {
+        gl: gl,
+        uniform: {
+            model_pos:  gl.getUniformLocation(program, 'model_pos'),
+            transform: gl.getUniformLocation(program, 'transform'),
+            rotation:   gl.getUniformLocation(program, 'rotation'),
+            projection: gl.getUniformLocation(program, 'projection'),
+            color:      gl.getUniformLocation(program, 'color')
+        }
+    };
+
+    gl.lineWidth(20);
+    gl.clearColor(0, 0, 0, 1);
+    gl.enable(gl.DEPTH_TEST);
+
+    models = {
+        [Dimension.TWO]: {
+            pieces: {
+                [Piece.X]: new Model(model_data.x[0]),
+                [Piece.O]: new Model(model_data.o[0]),
+            },
+            grid: new Model(model_data.grid[0])
+        },
+        [Dimension.THREE]: {
+            pieces: {
+                [Piece.X]: new Model(model_data.x[1]),
+                [Piece.O]: new Model(model_data.o[1]),
+            },
+            grid: new Model(model_data.grid[1])
+        },
+        [Dimension.FOUR]: {
+            pieces: {
+                [Piece.X]: new Model(model_data.x[2]),
+                [Piece.O]: new Model(model_data.o[2]),
+            },
+            grid: new Model(model_data.grid[2])
+        }
+    };
+
+    board.pieces = [Piece.X, Piece.O, Piece.X, Piece.O];
+
+    draw(board);
+}
+
+function draw(board: Board) {
+    const gl = ctx.gl;
+
+    const dimension = Math.min(camera.dimension, board.dimension);
+    const dim_models = models[dimension];
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    switch(dimension) {
+        case Dimension.TWO:
+            gl.uniformMatrix4fv(ctx.uniform.transform, false, identity());
+            gl.uniformMatrix4fv(ctx.uniform.projection, false, [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+            gl.uniformMatrix4fv(ctx.uniform.rotation, false, identity());
+            break;
+        case Dimension.THREE:
+            gl.uniformMatrix4fv(ctx.uniform.transform, false, mul(perspective(config.fov, 1, 0.1, 100), translate(0, 0, -camera.distance)));
+            gl.uniformMatrix4fv(ctx.uniform.projection, false, identity());
+            gl.uniformMatrix4fv(ctx.uniform.rotation, false, mul(rotate(camera.rotation[1], [1, 0, 0]), rotate(camera.rotation[0], [0, 1, 0])));
+            break;
+    }
+
+    dim_models.grid.draw([0, 0, 0, 0]);
+
+    board.pieces.forEach((piece, i) => {
+        const div = (a, b) => (a-a%b) / b;
+
+        if(piece != Piece.EMPTY) {
+
+            const x = i % 3;
+            const y = div(i % 9, 3);
+            const z = div(i % 27, 9);
+            const w = div(i, 27);
+
+            const pos = [
+                (x - 1) * 2/3,
+                (y - 1) * 2/3,
+                (z - 1) * 2/3,
+                (w - 1) * 2/3
+            ] as Vec4;
+
+            dim_models.pieces[piece].draw(pos);
+        }
+    });
+}
+
+function mousemove(e: MouseEvent) {
+    if(e.buttons & 1) {
+        camera.rotation[0] += config.rot_speed * e.movementX;
+        camera.rotation[1] += config.rot_speed * e.movementY;
+
+        if(camera.rotation[1] > Math.PI/2)
+            camera.rotation[1] = Math.PI/2;
+        if(camera.rotation[1] < -Math.PI/2)
+            camera.rotation[1] = -Math.PI/2;
+
+        draw(board);
+    }
+}
+
+function wheel(e: WheelEvent) {
+    camera.distance += config.zoom_speed * e.deltaY;
+    if(camera.distance < 0)
+        camera.distance = 0;
+    draw(board);
+}
